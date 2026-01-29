5d81f8907ac92d2023afd039c0953c29e51f3185
“shylockwolf” <shylockwolf@yahoo.com>
2026-01-29 23:45:06 +0800

    1.0.0  单个文件下载成功

---
+++ myYouTbubeDownloader/myYouTbubeDownloader/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  myYouTbubeDownloader
+//
+//  Created by Shylock Wolf on 2026/1/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ myYouTbubeDownloader/myYouTbubeDownloader/myYouTbubeDownloaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  myYouTbubeDownloaderApp.swift
+//  myYouTbubeDownloader
+//
+//  Created by Shylock Wolf on 2026/1/29.
+//
+
+import SwiftUI
+
+@main
+struct myYouTbubeDownloaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
