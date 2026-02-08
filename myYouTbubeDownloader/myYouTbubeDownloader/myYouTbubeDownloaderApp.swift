aec61bc7bd6b4d93803d421af0c878b13935272e
“shylockwolf” <shylockwolf@yahoo.com>
2026-02-08 10:15:28 +0800

    1.6.2 git catch up

--- myYouTbubeDownloader/myYouTbubeDownloader/myYouTbubeDownloaderApp.swift
+++ myYouTbubeDownloader/myYouTbubeDownloader/myYouTbubeDownloaderApp.swift
@@ -12,10 +12,7 @@ struct myYouTbubeDownloaderApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .frame(minWidth: 1000, minHeight: 600)
         }
-        .defaultSize(width: 1100, height: 650)
-        .windowResizability(.contentMinSize)
-        .windowStyle(.automatic)
+        .windowResizability(.contentSize)
     }
 }
